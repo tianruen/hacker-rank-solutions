ce3999087e5452724aec9cfeabf389cc11fb14aa
Tian Ruen Woon <105955490+tianruen@users.noreply.github.com>
2022-10-13 01:44:08 +0100

    Delete designer-pdf-viewer.py

--- designer-pdf-viewer.py
+++
@@ -1,8 +0,0 @@
-def designerPdfViewer(h, word):
-    a = ('abcdefghijklmnopqrstuvwxyz')
-    d = dict(zip(a,h))
-    x = []
-    for i in word:
-        x.append(d[i])
-    
-    return len(word)*max(x)
